654ff58d56c1d5120b68af7684a8bc32f820646e
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-04-05 15:51:36 +0200

    Add forms in a separated file

---
+++ auctions/forms.py
@@ -0,0 +1,95 @@
+from django import forms
+
+
+class AuctionListingForm(forms.Form):
+    title = forms.CharField(
+        label='Title',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control form-group',
+            'placeholder': 'Give it a title'
+        }
+        )
+    )
+    description = forms.CharField(
+        label='Description',
+        required=True,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control form-group',
+            'placeholder': 'Tell more about the product',
+            'rows': '3'
+        }
+        )
+    )
+    price = forms.DecimalField(
+        label='Price',
+        required=False,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control form-group',
+            'placeholder': 'Estimated price (optional)',
+            'min': '0.01',
+            'max': '999999999.99',
+            'step': '0.01'
+        }
+        )
+    )
+    starting_bid = forms.DecimalField(
+        label='Starting Bid',
+        required=True,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control form-group',
+            'placeholder': 'Starting bid',
+            'min': '0.01',
+            'max': '99999999999.99',
+            'step': '0.01'
+        }
+        )
+    )
+    category = forms.CharField(
+        label='Category',
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control form-group',
+            'autocomplete': 'on',
+            'placeholder': 'Category (optional)'
+        }
+        )
+    )
+    image_url = forms.URLField(
+        label='Image URL',
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control form-group',
+            'placeholder': 'Image URL (optional)'
+        }
+        )
+    )
+
+    def clean_starting_bid(self):
+        amount = self.cleaned_data.get('starting_bid')
+        if isinstance(amount, float) and amount > 0:
+            return amount
+        raise forms.ValidationError('Should be a number larger than zero!')
+
+    def clean_category(self):
+        category = self.cleaned_data.get('category')
+        return category.lower()
+
+
+class CommentForm(forms.Form):
+    text = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control-md lead form-group',
+            'rows': '3',
+            'cols': '100'
+        }
+        )
+    )
+
+    def clean_comment(self):
+        text = self.cleaned_data.get('text')
+        if len(text) > 0:
+            return text
+        return self.errors
