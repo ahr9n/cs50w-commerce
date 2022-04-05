41b9fa9b14d53cd07f24c8dd8b390973187b7f9d
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-04-05 21:42:46 +0200

    Finish beta-version with silly bugs :)

--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
-from .models import User
+from .models import User, AuctionListing, Bid, Comment
+from .forms import AuctionListingForm, CommentForm
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "listings": AuctionListing.objects.all()
+    })
 
 
 def login_view(request):
@@ -61,3 +66,127 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+# ======================================================================================================================
+@login_required(login_url='auctions/login.html')
+def create(request):
+    return render(request, "auctions/create.html", {
+        'form': AuctionListingForm()
+    })
+
+
+@login_required(login_url='auctions/login.html')
+def insert(request):
+    form = AuctionListingForm(request.POST)
+    if form.is_valid():
+        auction = AuctionListing(author=request.user, **form.cleaned_data)
+        auction.save()
+        starting_bid = auction.starting_bid
+        bid = Bid(amount=starting_bid, user=request.user, auction=auction)
+        bid.save()
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'auctions/create.html', {
+            'form': form,
+            'error': form.errors
+        })
+
+
+def listing(request, id):
+    current = AuctionListing.objects.get(pk=id)
+    bid = get_object_or_404(Bid, auction=current)
+    comments = Comment.objects.filter(auction=current)
+    return render(request, 'auctions/listing.html', {
+        'auction': current,
+        'user': request.user,
+        'bid': bid,
+        'comments': comments,
+        'comment_form': CommentForm()
+    })
+
+
+@login_required(login_url='auctions/login.html')
+def update_bid(request, id):
+    amount = request.POST['bid']
+    if amount:
+        amount = float(amount)
+        auction = get_object_or_404(AuctionListing, id=id)
+        if amount > get_object_or_404(Bid, id=id).amount:
+            bid = get_object_or_404(Bid, id=id)
+            bid.user, bid.amount = request.user, amount
+            bid.save()
+            auction.bid_counter += 1
+            auction.save()
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            raise ValidationError('Bid must be greater than current Bid value')
+    else:
+        raise ValidationError('Bid must be greater than current Bid value')
+
+
+@login_required(login_url='auctions/login.html')
+def close_bid(request, id):
+    auction = get_object_or_404(AuctionListing, id=id)
+    auction.active, auction.winner = False, request.user
+    auction.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+@login_required(login_url='auctions/login.html')
+def watchlist(request):
+    return render(request, "auctions/watchlist.html", {
+        "watchlist": request.user.watchlist.all()
+    })
+
+
+@login_required(login_url='auctions/login.html')
+def watch(request, id):
+    auction = get_object_or_404(AuctionListing, id=id)
+    request.user.watchlist.add(auction)
+    request.user.watchlist_counter += 1
+    request.user.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+@login_required(login_url='auctions/login.html')
+def unwatch(request, id):
+    auction = get_object_or_404(AuctionListing, id=id)
+    request.user.watchlist.remove(auction)
+    request.user.watchlist_counter -= 1
+    request.user.save()
+    if '/unwatch/' in request.path:
+        return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('wishlist'))
+
+
+def categories(request):
+    return render(request, "auctions/categories.html")
+
+
+def filter(request):
+    q = request.GET['category'].lower()
+    return render(request, 'auctions/category.html', {
+        'listings': AuctionListing.objects.filter(category=q)
+    })
+
+
+def add_comment(request, id):
+    anonymous = User.first_name
+    if request.user is not anonymous:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            f = form.cleaned_data
+            comment = Comment(
+                user=request.user,
+                auction=get_object_or_404(AuctionListing, id=id),
+                **f
+            )
+            comment.save()
+            return HttpResponseRedirect(reverse('listing', kwargs={
+                'id': id
+            }))
+    else:
+        return render(request, 'auctions/login.html', {
+            'message': 'Must be logged in to be able to comment!'
+        })
