75ee8896d736ec10a3473554e789a0454412ae2b
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-04-15 14:35:36 +0200

    Add Django-Heroku

--- commerce/settings.py
+++ commerce/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+
+import django_heroku
+django_heroku.settings(locals())
