897ba8a16b459bab7364c5c3b44c836aae3b2920
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-04-15 14:54:38 +0200

    Fix bugs

--- commerce/settings.py
+++ commerce/settings.py
@@ -133,5 +133,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configure Django App for Heroku.
 
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
+
+# No need to thess lines :)
