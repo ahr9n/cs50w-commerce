46b178bd5df2c5b4d27fd856e3a37f75ad0c94ae
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-04-15 14:59:48 +0200

    Import whitenoise

--- commerce/settings.py
+++ commerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+import whitenoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
